1f1ea6da042993d1e4c6609aa4520560eda0fd4b
Ayşenur Asya Aktürk <aasyaakturk@gmail.com>
2022-10-20 01:40:04 +0300

    yukleme

---
+++ main.py
@@ -0,0 +1,22 @@
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+img = cv2.imread("Phantom4RTK_2.jpg")
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+stop_data = cv2.CascadeClassifier('cascade.xml')
+found = stop_data.detectMultiScale(img_gray,minSize=(20,20))
+amount_found =len(found)
+
+if amount_found != 0:
+    for (x, y, width, height) in found:
+        cv2.rectangle(img_rgb, (x, y),
+                      (x + height, y + width),
+                      (0, 255, 0), 5)
+plt.subplot(1, 1, 1)
+plt.imshow(img_rgb)
+plt.show()
+
+
+
+
